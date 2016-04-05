e9aa0ece1c1f9a62d254453e50641e096f6cc196
bacho <bachosh2@gmail.com>
2016-04-05 13:07:04 +0000

    5 aprili leqciis  gagrdzeleba

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ActiveRecord::Base
+  
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
     
     def self.find_by_ticker(ticker_symbol)   # bazashi edzebs stoks cxrilshi
       where(ticker: ticker_symbol).first
